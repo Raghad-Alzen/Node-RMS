c07b26043e8b955114a36795f020a8ae26988827
Raghad-Alzen <raghadzn1995@gmail.com>
2023-08-03 18:23:19 +0300

    first commit

--- route/customerRoute.js
+++ route/customerRoute.js
@@ -13,5 +13,41 @@ router.post("/signupCustomer", async (request, response) => {
     }
   });
 
+
+  router.get("/get_allCustomer", async (request, response) => {
+    try {
+      const customer = await Customer.find({}).select("email");
+      response.status(200).json(customer);
+    } catch (error) {
+      console.log(error.message);
+      response.status(500).json({ message: error.message });
+    }
+  });
+   
+
+  router.put("/updateCustomer_info/:id", async (request, response) => {
+    try {
+      const { id } = request.params;
+      const customer = await Customer.findByIdAndUpdate(id, request.body);
+      if(!customer)
+      response
+      .status(404)
+      .json({ message: 'cannot find user with id ${id} !'});
+    else {
+      const newcustomer = await Customer.findById(id);
+      response.status(200).json(newcustomer);
+    }  
+    } catch (error) {
+      console.log(error.message);
+      response.status(500).json({ message: error.message});
+    }
+  });
+
+
+
+
+
+
+
   
   module.exports = router;
